4874acaf4a771c4a9765c4c8245b3009c4e0536d
Polina Koleva <polly.akoleva@gmail.com>
2013-02-16 13:52:15 +0200

    Test

---
+++ Problem1MethodHello/Problem1MethodHello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//Write a method that asks the user for his name
+//and prints “Hello, <name>” (for example, “Hello, Peter!”). 
+//Write a program to test this method.
+
+class Problem1MethodHello
+{
+    static void Greetings()
+    {
+        Console.WriteLine("What`s your name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, {0}!", name);
+    }
+    static void Main()
+    {
+        Greetings();
+    }
+}
+
